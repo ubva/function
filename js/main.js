51feb4288bedf27aeffd91be8237a88650f924ea
ubva <aubaydullayevaa4@gmail.com>
2022-11-23 14:28:28 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let inp = document.querySelector('.main-inp')
+let btn = document.querySelector('.btn')
+let itemVar1 = document.querySelector('.item-var1')
+let itemVar2 = document.querySelector('.item-var2')
+let itemVar3 = document.querySelector('.item-var3')
+let itemVar4 = document.querySelector('.item-var4')
+
+
+btn.addEventListener('click' , function(){
+    let inpValue = inp.value.split('km')[0]
+  
+    function varBtn (p){
+        return p
+    }
+   itemVar1.textContent = (inpValue / varBtn(3.6)).toFixed(0)
+
+   function varBtn (b){
+    return b
+    }
+    itemVar2.textContent = (inpValue / varBtn(20.1)).toFixed(0)
+
+    function varBtn (c){
+        return c
+    }
+    itemVar3.textContent = (inpValue / varBtn(70)).toFixed(0)
+
+    function varBtn (p){
+        return p
+    }
+    itemVar4.textContent = (inpValue / varBtn(800)).toFixed(0)
+    }
+    )
+
+
